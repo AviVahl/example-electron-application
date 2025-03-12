f5a494e3dd04d5c7084501ac8f0bb8e6828929f2
Avi Vahl <avi@vahl.co.il>
2025-03-12 12:53:36 +0200

    fix(scripts): ensure window loads before screenshot needed for electron 35.0.1's font loading

--- scripts/generate-screenshots.js
+++ scripts/generate-screenshots.js
@@ -6,6 +6,7 @@ import { _electron as electron } from "playwright";
 for (const colorScheme of /** @type {const} */ (["light", "dark"])) {
   const app = await electron.launch({ args: ["."], colorScheme });
   const window = await app.firstWindow();
+  await window.waitForLoadState("load");
   await window.screenshot({
     type: "png",
     path: fileURLToPath(new URL(`../docs/screenshot-${colorScheme}.png`, import.meta.url)),
