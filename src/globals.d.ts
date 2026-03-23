ba8abba80b8505772c3f482a8e550bf299111b5b
Avi Vahl <avi@vahl.co.il>
2026-03-23 23:29:57 +0200

    chore(deps-dev): typescript@6.0.2 and regenerate lock file

--- src/globals.d.ts
+++ src/globals.d.ts
@@ -22,3 +22,8 @@ declare module "*.module.css" {
   const classes: Record<string, string>;
   export default classes;
 }
+
+declare module "*.css" {
+  const classes: never;
+  export default classes;
+}
